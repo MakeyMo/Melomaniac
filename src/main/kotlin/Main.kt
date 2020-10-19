d381db0a57f441b2bae7ae9bbd246159c2619f38
Alex Morozov <igroman-x@list.ru>
2020-10-19 23:26:28 +0300

    chabged to version 2.0

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,48 +1,52 @@
 fun main() {
 
     val purchaseAmount: Int = (0..20_000).random()
-    val songsToBuy: Int = (0..100).random()
+    val summOfBuy: Int = (0..10_000).random()
     val melomaniacStatus: Int = (0..2).random()
     val melomanoacDiscount: Double = 0.01
-    val songPrice: Int = 100
-    val summOfBuy: Int = songsToBuy * songPrice
 
-    fun checkDiscount(purchaseAmount: Int, songsToBuy: Int, songPrice: Int, melomaniacStatus: Int): Double {
+    fun checkRegularDiscount(purchaseAmount: Int, summOfBuy: Int): Double {
         if (purchaseAmount <= 1_000) {
             val discount: Double = 0.0
-            if (melomaniacStatus == 0) {
                 return discount
-            } else {
-                return songsToBuy.toDouble() * songPrice.toDouble() * melomanoacDiscount
-            }
         }
         else if (purchaseAmount <= 10_000) {
             val discount: Double = 100.0
-            if (melomaniacStatus == 0) {
-                return discount
-            } else {
-                return discount + (songsToBuy.toDouble() * songPrice.toDouble() - discount) * melomanoacDiscount
-            }
+            return discount
         }
-        else if (purchaseAmount >= 10_001) {
+        else {
             val discount: Double = 0.05
-            if (melomaniacStatus == 0) {
-                return songsToBuy.toDouble() * songPrice.toDouble() * discount
-            } else {
-                return (songsToBuy.toDouble() * songPrice.toDouble() * discount) +
-                        (songsToBuy.toDouble() * songPrice.toDouble() -
-                                songsToBuy.toDouble() * songPrice.toDouble() * discount) * melomanoacDiscount
-            }
+                return summOfBuy.toDouble() * discount
         }
-        return 0.0
     }
 
-    fun countFinalSumm(summOfBuy: Int): Double {
-        return summOfBuy - checkDiscount(purchaseAmount, songsToBuy, songPrice, melomaniacStatus)
+    fun summWithRegularDiscount(summOfBuy: Int): Double {
+        return summOfBuy - checkRegularDiscount(purchaseAmount, summOfBuy)
+    }
+
+    fun checkMelomaniacDiscount(summOfBuy: Int, melomaniacStatus: Int, melomaniacDiscount: Double): Double {
+        if (melomaniacStatus == 0) {
+            return 0.0
+        } else {
+            return summWithRegularDiscount(summOfBuy) * melomaniacDiscount
+        }
+    }
+
+    fun summWithMelomaniacDiscount(summOfBuy: Int, melomaniacDiscount: Double): Double {
+        return summWithRegularDiscount(summOfBuy) -
+                summWithRegularDiscount(summOfBuy) * melomaniacDiscount
     }
 
     println("Сумма вашей покупки составила $summOfBuy руб.")
-    println("Сумма вашей скидки составляет ${checkDiscount(purchaseAmount, songsToBuy, songPrice, melomaniacStatus)} "
+    println("Сумма вашей скидки составляет ${checkRegularDiscount(purchaseAmount, summOfBuy)} "
             + "руб.")
-    println("Итоговая стоимость покупки - ${countFinalSumm(summOfBuy)} руб.")
+    if (melomaniacStatus == 0) {
+        println("Итоговая стоимость покупки - " +
+                "${summWithRegularDiscount(summOfBuy)} руб.")
+    } else {
+        println("+ за регулярные покупки вы выполучаете дополнительную скидку 1% - ${checkMelomaniacDiscount(summOfBuy, 
+        melomaniacStatus, melomanoacDiscount)} руб.")
+        println("Итоговая стоимость покупки - " +
+                "${summWithMelomaniacDiscount(summOfBuy, melomanoacDiscount)} руб.")
+    }
 }
